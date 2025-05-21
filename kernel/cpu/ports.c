0972f022063cf2656b72fa06a50a202966c292b5
Francisco Araújo <franciscocoelhoaraujo492000@gmail.com>
2025-05-21 17:17:38 +0100

    updata ports

--- kernel/cpu/ports.c
+++ kernel/cpu/ports.c
@@ -1,6 +1,11 @@
 #include <ports.h>
 #include <stdint.h>
 
+void cpuid(uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx)
+{
+	asm volatile ("cpuid \n" : "=a"(*eax), "=b"(*ebx), "=c"(*ecx), "=d"(*edx) : "a"(*eax) : "memory");
+}
+
 void outportb(uint16_t port, uint8_t value)
 {
 	asm volatile("outb %1, %0" : : "dN"(port), "a"(value));
