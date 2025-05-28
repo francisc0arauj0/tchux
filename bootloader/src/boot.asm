c369590ef9466676429c4defee78ac598ee164ce
Francisco Araújo <franciscocoelhoaraujo492000@gmail.com>
2025-05-28 10:23:43 +0100

    add protected mode, GDT and kernel

--- bootloader/src/boot.asm
+++ bootloader/src/boot.asm
@@ -1,11 +1,20 @@
 org 0x7C00
-bits 16
+
+kernel_offset equ 0x1000
 
 start:
+	mov [boot_drive], dl
+	mov bp, 0x9000
+	mov sp, bp
 	jmp main
 
+
+%include "bootloader/src/gdt.asm"
+
+bits 16
+
 main:
-	; this causes the stack to stay in _0x0000:0x7C00
+	; this causes the stack to stay in 0x0000:0x7C00
 	mov ax, 0
 	mov ds, ax
 	mov es, ax
@@ -13,29 +22,60 @@ main:
 	mov sp, 0x7C00
 
 	; load sector 2 (LBA 1) to 0x1000
-	; mov ah, 0x02			; read sectors
-	; mov al, 1				; number of sectors to read
-	; mov ch, 0				; cylinder 0
-	; mov cl, 2				; sctor 2
-	; mov dh, 0				; header 0
-	; mov dl, [boot_drive]	; drive
-	; mov bx, 0x1000			; destination address
-	; int 0x13				; call BIOS to read from disk
+	mov ah, 0x02			; read sectors
+	mov al, 1				; number of sectors to read
+	mov ch, 0				; cylinder 0
+	mov cl, 2				; sctor 2
+	mov dh, 0				; header 0
+	mov dl, [boot_drive]	; drive
+	mov bx, kernel_offset	; destination address
+	int 0x13				; call BIOS to read from disk
 	
-	; jc disk_error			; if CF is set, error
+	jc disk_error			; if CF is set, error
 	
-	; jmp 0x0000:0x1000		; jump to the loaded "kernel"
+	cli						; disable interrupts
+	
+	; prepare GDT
+	lgdt [gdt_descriptor]
+	
+	; enable protected mode (CR0.PE = 1)
+	mov eax, cr0
+	or eax, 1
+	mov cr0, eax
+	
+	; jump to protected mode 
+	jmp code_segment:protected_mode_start 
+	
+disk_error:
 	hlt
-	
-;disk_error:
-;	hlt
-;	jmp $
+	jmp $
 
-;boot_drive:
-;	db 0
+boot_drive:
+	db 0
+
+
+bits 32
+
+protected_mode_start:
+	mov ax, data_segment
 	
-.halt:
-	jmp .halt
+	mov ds, ax
+	mov ss, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
 	
+	mov ebp, 0x90000
+	mov esp, ebp
+	
+	call begin_protected_mode
+
+begin_protected_mode:
+	call kernel_offset
+	jmp $
+	
+code_segment equ gdt_code - gdt_start
+data_segment equ gdt_data - gdt_start
+
 times 510-($-$$) db 0
 dw 0xAA55
--- makefile
+++ makefile
@@ -1,15 +1,19 @@
 ASM = nasm
 BOOTLOADER_SRC = bootloader/src
+KERNEL_SRC = kernel
 BUILD_DIR = build
 
+ 
+$(BUILD_DIR)/main_floppy.img: $(BUILD_DIR)/bootloader.bin $(BUILD_DIR)/kernel.bin
+	dd if=/dev/zero of=build/main_floppy.img bs=512 count=2880
+	dd if=$(BUILD_DIR)/bootloader.bin of=$(BUILD_DIR)/main_floppy.img conv=notrunc
+	dd if=$(BUILD_DIR)/kernel.bin of=$(BUILD_DIR)/main_floppy.img bs=512 seek=1 conv=notrunc
 
-$(BUILD_DIR)/main_floppy.img: $(BUILD_DIR)/bootloader.bin
-	cp $(BUILD_DIR)/bootloader.bin $(BUILD_DIR)/main_floppy.img
-	# dd if=/dev/zero of=$(BUILD_DIR)/main_floppy.img bs=512 count=2880
-	truncate -s 1440k $(BUILD_DIR)/main_floppy.img
+$(BUILD_DIR)/bootloader.bin: $(BOOTLOADER_SRC)/boot.asm $(BOOTLOADER_SRC)/gdt.asm
+	$(ASM) $< -f bin -o $@
 
-$(BUILD_DIR)/bootloader.bin: $(BOOTLOADER_SRC)/boot.asm
-	$(ASM) $(BOOTLOADER_SRC)/boot.asm -f bin -o $(BUILD_DIR)/bootloader.bin
+$(BUILD_DIR)/kernel.bin: $(KERNEL_SRC)/kernel.asm
+	$(ASM) $< -f bin -o $@
 
     
-# qemu-system-i386 -m 2G -monitor stdio -d int -M smm=off -fda build/main_floppy.img 
+# qemu-system-i386 -m 2G -monitor stdio -d int -M smm=off -fda build/main_floppy.img -boot a
